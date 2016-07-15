cb1c3a909290f8f56bdcf80ba1026b8bb58fb0c1
Tojokely <rtojosoa15@alustudent.com>
2016-07-15 13:26:16 +0000

    final b

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes/index.js');
+var port = process.env.PORT || 8080;
 
 routes(app);
 
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 8080!');
 });
